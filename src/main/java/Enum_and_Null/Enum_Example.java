599bdb1b3528e31945afd6308cc1379a6f805d17
Salma <95961672+Tabrijix86@users.noreply.github.com>
2024-01-26 01:01:07 +0600

    more

---
+++ src/main/java/Enum_and_Null/Enum_Example.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Enum_and_Null;
+
+/**
+ *
+ * @author tabri
+ */
+public class Enum_Example {
+    
+}
