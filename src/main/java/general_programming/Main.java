5c14321741e383603ef9ba89383dab40a2376546
Salma <95961672+Tabrijix86@users.noreply.github.com>
2024-01-26 00:48:12 +0600

    Update Main.java

--- src/main/java/general_programming/Main.java
+++ src/main/java/general_programming/Main.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package general_programming;
 
 /**
@@ -10,6 +6,6 @@ package general_programming;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Srping 2024!");
+        System.out.println("Hello, People of Srping 2024!");
     }
 }
